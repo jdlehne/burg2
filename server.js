dfd4519f8a9f4d29f6546b06276a4449bcb787a9
Josh Lehne <jdlehne@gmail.com>
2017-10-06 11:57:09 -0500

    cleaning code

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var exphbs = require("express-handlebars");
-
-
 
 var app = express();
 var PORT= process.env.PORT || 7000;
@@ -22,10 +19,6 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(methodOverride("_method"));
-app.engine("handlebars", exphbs({
-  defaultLayout: "main"
-}));
-app.set("view engine", "handlebars");
 
 require("./routes/api-routes.js")(app);
 
